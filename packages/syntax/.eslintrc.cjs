c6c2316a16bff65297ec78bacc90f2af55fd750a
bytasv <vytautas.butkus@gmail.com>
2022-10-20 08:27:40 +0300

    Allow using underscore as non used var name

--- packages/syntax/.eslintrc.cjs
+++ packages/syntax/.eslintrc.cjs
@@ -100,6 +100,6 @@ module.exports = {
       }
     ],
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error'
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
   }
 }
